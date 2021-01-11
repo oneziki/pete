cde99ba06e1c979833749ade371c8bfa5469d084
Onesmus M Ziki <32875198+oneziki@users.noreply.github.com>
2021-01-11 08:28:08 +0200

    2021-01-10
    Working Back-up before uploading to the live server

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'pete' );
+define( 'DB_NAME', 'db_pete' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'onesmus' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'BastosZC1*#' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
--- wp-content/themes/mytheme/template-parts/home_slider.php
+++ wp-content/themes/mytheme/template-parts/home_slider.php
@@ -5,45 +5,51 @@
 			</div>
 			<div class="container">
 				<div class="slide-content">
-					<h4 class="optinal-h4">Top Level</h4>
-					<h3>Compassionate Care that you can trust</h3>
-					<a href="#" class="btn btn-primary">Meet our Team</a>
+					<h4 class="optinal-h4"><?php the_field('top_level');?>Top Level</h4>
+					<h3><?php the_field('compassionate_text');?>Compassionate Care that you can trust</h3>
+					<a href="#" class="btn btn-primary"><?php the_field('meet_our_team');?>Meet our Team</a>
 				</div>
 			</div>
 		</div>
+
+
 		<div class="item">
 			<div class="slide-image">
 				<img src="./wp-content/themes/mytheme/webfx/assets/images/slider-img2.jpg" alt="Slide" />
 			</div>
 			<div class="container">
 				<div class="slide-content">
-					<h4 class="optinal-h4">We are</h4>
-					<h3>accepting new patients</h3>
-					<a href="#" class="btn btn-primary">New Client Special</a>
+					<h4 class="optinal-h4"><?php the_field('we_are');?>We are</h4>
+					<h3><?php the_field('accepting_new_patients');?>accepting new patients</h3>
+					<a href="#" class="btn btn-primary"><?php the_field('new_client_special');?>New Client Special</a>
 				</div>
 			</div>
 		</div>
+
+
 		<div class="item">
 			<div class="slide-image">
 				<img src="./wp-content/themes/mytheme/webfx/assets/images/slider-img3.jpg" alt="Slide" />
 			</div>
 			<div class="container">
 				<div class="slide-content">
-					<h4 class="optinal-h4">Early detection to</h4>
-					<h3>help your pet live a longer & happier life</h3>
-					<a href="#" class="btn btn-primary">See Our Wellness Plans</a>
+					<h4 class="optinal-h4"><?php the_field('early_detection_to');?>Early detection to</h4>
+					<h3><?php the_field('help_your_pet');?>help your pet live a longer & happier life</h3>
+					<a href="#" class="btn btn-primary"><?php the_field('see_our');?>See Our Wellness Plans</a>
 				</div>
 			</div>
 		</div>
+
+
 		<div class="item">
 			<div class="slide-image">
 				<img src="./wp-content/themes/mytheme/webfx/assets/images/slider-img4.jpg" alt="Slide" />
 			</div>
 			<div class="container">
 				<div class="slide-content">
-					<h4 class="optinal-h4">veterinary</h4>
-					<h3>care for your exotic pet</h3>
-					<a href="#" class="btn btn-primary">View Types of Animals We Treat</a>
+					<h4 class="optinal-h4"><?php the_field('veterinary_text');?>veterinary</h4>
+					<h3><?php the_field('care_for_your');?>care for your exotic pet</h3>
+					<a href="#" class="btn btn-primary"><?php the_field('view_types');?>View Types of Animals We Treat</a>
 				</div>
 			</div>
 		</div>
